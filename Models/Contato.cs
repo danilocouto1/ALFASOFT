50cb60145027209a59bf23e5e18c089f11e13e19
Dan <dansantosalfa@gmail.com>
2023-07-27 09:16:00 -0300

    Criando Model  Contato

---
+++ Models/Contato.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ALFASOFT.Models
+{
+    public class Contato
+    {
+        public Guid ID { get; set; }
+
+        [Required]
+        [StringLength(255, MinimumLength = 6)]
+        public string Nome { get; set; }
+
+        [Required]
+        [StringLength(9, MinimumLength = 9)]
+        public string ContatoNumero { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}

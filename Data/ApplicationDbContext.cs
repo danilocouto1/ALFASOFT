2f6670e776bb157b4bcab89111e983c46d3ff67c
Dan <dansantosalfa@gmail.com>
2023-07-27 14:53:37 -0300

    Adicionando o DbContext

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using ALFASOFT.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ALFASOFT.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { }
+
+        public DbSet<Contato> Contato { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Garante restrições únicas para os campos Contato e Email
+            modelBuilder.Entity<Contato>()
+                .HasIndex(c => c.ContatoNumero)
+                .IsUnique();
+
+            modelBuilder.Entity<Contato>()
+                .HasIndex(c => c.Email)
+                .IsUnique();
+        }
+    }
+}

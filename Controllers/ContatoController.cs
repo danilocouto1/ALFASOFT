b1b1ede9f0894062c33b8df004a77838c08cb350
Dan <dansantosalfa@gmail.com>
2023-07-27 14:55:54 -0300

    Gerando Controllers pelo atalho do visual studio

---
+++ Controllers/ContatoController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ALFASOFT.Data;
+using ALFASOFT.Models;
+
+namespace ALFASOFT.Controllers
+{
+    public class ContatoController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ContatoController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Contato
+        public async Task<IActionResult> Index()
+        {
+              return _context.Contato != null ? 
+                          View(await _context.Contato.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.Contato'  is null.");
+        }
+
+        // GET: Contato/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Contato == null)
+            {
+                return NotFound();
+            }
+
+            var contato = await _context.Contato
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (contato == null)
+            {
+                return NotFound();
+            }
+
+            return View(contato);
+        }
+
+        // GET: Contato/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Contato/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Nome,ContatoNumero,Email")] Contato contato)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(contato);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(contato);
+        }
+
+        // GET: Contato/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Contato == null)
+            {
+                return NotFound();
+            }
+
+            var contato = await _context.Contato.FindAsync(id);
+            if (contato == null)
+            {
+                return NotFound();
+            }
+            return View(contato);
+        }
+
+        // POST: Contato/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Nome,ContatoNumero,Email")] Contato contato)
+        {
+            if (id != contato.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(contato);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ContatoExists(contato.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(contato);
+        }
+
+        // GET: Contato/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Contato == null)
+            {
+                return NotFound();
+            }
+
+            var contato = await _context.Contato
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (contato == null)
+            {
+                return NotFound();
+            }
+
+            return View(contato);
+        }
+
+        // POST: Contato/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Contato == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Contato'  is null.");
+            }
+            var contato = await _context.Contato.FindAsync(id);
+            if (contato != null)
+            {
+                _context.Contato.Remove(contato);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ContatoExists(int id)
+        {
+          return (_context.Contato?.Any(e => e.ID == id)).GetValueOrDefault();
+        }
+    }
+}

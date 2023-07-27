7267a742874740b7d9697b6257cefeedce212606
Dan <dansantosalfa@gmail.com>
2023-07-27 14:55:01 -0300

    Atualizando contatos

--- Models/Contato.cs
+++ Models/Contato.cs
@@ -1,21 +1,29 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ALFASOFT.Models
 {
+    [Table("Contato")]
     public class Contato
     {
-        public Guid ID { get; set; }
+        [Required]
+        [Column("ID")]
+        [Key]
+        public int ID { get; set; }
 
         [Required]
         [StringLength(255, MinimumLength = 6)]
+        [Column("Nome")]
         public string Nome { get; set; }
 
         [Required]
         [StringLength(9, MinimumLength = 9)]
+        [Column("ContatoNumero")]
         public string ContatoNumero { get; set; }
 
         [Required]
         [EmailAddress]
+        [Column("Email")]
         public string Email { get; set; }
     }
 }

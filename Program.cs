6e39be043a8e2e85e40bc65dc5161570c2887aca
Dan <dansantosalfa@gmail.com>
2023-07-27 14:54:44 -0300

    Estruturando o Program.cs e appsettings

--- Program.cs
+++ Program.cs
@@ -1,10 +1,21 @@
+using ALFASOFT.Data;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var serverVersion = new MySqlServerVersion(new Version(8, 0, 28));
+
+var connection = builder.Configuration["ConnectionString:MariaDB"];
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(connection, serverVersion ?? throw new InvalidOperationException("Connection string 'MariaDB' not found.")));
+
+
+
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
 builder.Services.AddRazorPages();
-
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
